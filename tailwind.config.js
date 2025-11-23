b0efc329cde5734284324cb39a5ade095ca4c2ed
FUAD487 <fuadabdullahmahi@gmail.com>
2025-11-23 17:47:08 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 15s linear infinite',
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
